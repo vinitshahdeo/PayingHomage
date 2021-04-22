1f683c1ea39532f2b22889a0ee206725353cbc60
Souvik De <souvikde.ns@gmail.com>
2021-04-22 20:18:53 +0530

    connecting google-sheets api

--- client/src/components/Banner/containerHook.js
+++ client/src/components/Banner/containerHook.js
@@ -1,11 +1,23 @@
+import React, {useEffect} from 'react';
 import trees from '../../assets/trees.json';
+import {GoogleSpreadsheet} from 'google-spreadsheet'
 
 export default function useBanner() {
 
+    const fetchTrees = async () => {
+        const doc = new GoogleSpreadsheet(process.env.REACT_APP_GOOGLE_SHEET_ID);
+        
+    }
+
+    useEffect(() => {
+        
+    })
+
     function getTrees() {
+        // TODO: Fetch data from google API and update the number of trees for the counter.
         const year = new Date().getFullYear();
         return 1000;
-        return trees.filter((t) => t.year <= year).map((t) => t.trees).reduce((result, value) => result + value);
+        // return trees.filter((t) => t.year <= year).map((t) => t.trees).reduce((result, value) => result + value);
     }
 
     return {
