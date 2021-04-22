22f5576856c1a75fd253dace714272030dfb493e
Vinit Shahdeo <vinit.shahdeo@postman.com>
2021-04-22 16:35:16 +0530

    made headings bolder

--- client/src/components/AboutVivek/AboutVivek.js
+++ client/src/components/AboutVivek/AboutVivek.js
@@ -31,7 +31,7 @@ const AboutVivek = () => {
       <Col md={6} className="d-flex align-items-stretch">
         <VivAbout className="d-flex justify-content-center">
           <div className="text">
-            <h2 className='headings'>RIP Sir Vivek</h2>
+            <h2 className='headings'><span>RIP Sir Vivek</span></h2>
             <p className='about__vivek'>
               Vivekanandan, better known as Vivek, Indian film actor, comedian,
               Television personality, playback singer and versatile social
@@ -42,7 +42,7 @@ const AboutVivek = () => {
               University, has conferred, Vivek with an honorary doctorate for
               his contribution to the society through cinema.
             </p>
-            <h2 className='headings'>Paying tribute</h2>
+            <h2 className='headings'><span>Paying tribute</span></h2>
             <p className='about__vivek'>
             We are thinking in similar lines as many of Actor Vivek's fans. We would like to document the trees planting efforts by the fans so that we will keep a counter to confirm that we reached 1,00,00,000 plus trees planted in memory of Actor Padma Shri Vivek's initiative to plant trees as advised by his Guru Dr. Abdul Kalam few years back. We are trying to carry the mantle that Vivek Sir had left with us. Do help us in carrying the mantle forward through planting of Trees! 
             </p>
