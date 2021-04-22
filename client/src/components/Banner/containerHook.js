28fb0abeaa50d5c16f13004c1eb290b8944d54a1
Vinit Shahdeo <vinit.shahdeo@postman.com>
2021-04-22 16:14:27 +0530

    UMinor copy changes

--- client/src/components/Banner/containerHook.js
+++ client/src/components/Banner/containerHook.js
@@ -4,6 +4,7 @@ export default function useBanner() {
 
     function getTrees() {
         const year = new Date().getFullYear();
+        return 1000;
         return trees.filter((t) => t.year <= year).map((t) => t.trees).reduce((result, value) => result + value);
     }
 
